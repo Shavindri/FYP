6f00155d3aaca91867e47fa40f75cc151d03fd37
Shavindri <69174853+Shavindri@users.noreply.github.com>
2026-07-15 00:24:20 +0100

    Update app (3).py

--- app (3).py
+++ app (3).py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-import mediapipe as mp
 
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
